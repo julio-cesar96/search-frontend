5079b7ea42ed8376850c2505a469ad655a9eafb5
julio-cesar96 <juliobritods.96@gmail.com>
2025-07-31 13:57:07 -0300

    feat: add route to product detail page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "../pages/HomePage";
 import ProductListPage from "../pages/ProductListPage";
+import ProductDetailPage from "../pages/ProductDetailPage";
 
 export default function AppRoutes() {
   return (
@@ -9,7 +10,7 @@ export default function AppRoutes() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<ProductListPage />} />
-        {/* <Route path="/products/:id" element={<ProductDetailPage />} /> */}
+        <Route path="/products/:id" element={<ProductDetailPage />} />
       </Routes>
     </BrowserRouter>
   );
